53994968ecf6bcf583ad41e0dfbd09ab6b4391b4
kjstyle <kjstyle79@naver.com>
2020-12-27 16:34:37 +0900

    그리드뷰 만들기 * 안드로이드 스튜디오 4.1 부터 kotlin-android-extensions 플러그인 수동 추가 필요 * getView의 구현체 변수명이 강의와 다름 -> 헤깔리리지만 시그니처가 같기땜에 패스 * 강사의 변수명 네이밍을 보면 협업보다는 혼자 많이 코딩한 느낌이 강하다 * style이 themes라는 걸로 변경이 되었다고하고, 다크버전용이 따로 있어서 두벌을 동시 관리해야함

--- app/src/main/java/com/example/comin/MainActivity.kt
+++ app/src/main/java/com/example/comin/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.comin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.viewpager.widget.ViewPager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,6 +12,40 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val imgs = arrayOf(
+                R.drawable.ai,
+                R.drawable.css,
+                R.drawable.html,
+                R.drawable.jpg,
+                R.drawable.js,
+                R.drawable.mp4,
+                R.drawable.pdf,
+                R.drawable.php,
+                R.drawable.png,
+                R.drawable.tiff,
+                R.drawable.profile,
+                R.drawable.id
+        )
+
+        val texts = arrayOf(
+                "ai",
+                "css",
+                "html",
+                "jpg",
+                "js",
+                "mp4",
+                "pdf",
+                "php",
+                "pgn",
+                "tiff",
+                "profile",
+                "id"
+        )
+
+        val gridviewAdapter = GridviewAdapter(this, imgs, texts)
+        gridview.adapter = gridviewAdapter
+
 
         viewPager = findViewById(R.id.viewpager) as ViewPager
         val adapter = ViewPagerAdapter(this)
